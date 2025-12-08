055dc94de236ac9dd13b01c8eaec9c62c0aa594a
Dmitry <blakethespend@gmail.com>
2025-12-08 10:09:34 +0300

    corrected db & models structure

--- rsl/hello/models.py
+++ rsl/hello/models.py
@@ -1,24 +1,20 @@
 from django.db import models
-from django.contrib.auth import get_user_model
-from django.contrib.contenttypes.fields import GenericForeignKey
-from django.contrib.contenttypes.models import ContentType
+
 
 # Получаем модель пользователя, которая используется в проекте
-User = get_user_model()
 
 
 class Comment(models.Model):
     """
-    Модель для хранения комментариев.
-    Использует GenericForeignKey для привязки к любому объекту (статье, продукту и т.д.).
+    Модель для хранения комментариев
     """
 
-    # 3. Текст комментария
-    text = models.TextField(verbose_name="Текст комментария")
+    commentText = models.TextField(verbose_name="Текст комментария")
+    pub_date = models.DateTimeField("date published", null=True)
 
     class Meta:
         verbose_name = "Комментарий"
         verbose_name_plural = "Комментарии"
 
     def __str__(self):
-        return f"Комментарий f{self.text}"
+        return f"Комментарий {self.commentText}"
