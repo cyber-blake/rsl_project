6fc90d6156c2d7f4c958d824042546e568a4e8f4
Dmitry <blakethespend@gmail.com>
2026-03-10 13:51:48 +0300

    Tinkering

--- rsl/rsl/settings.py
+++ rsl/rsl/settings.py
@@ -152,8 +152,6 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 MEDIA_URL = "media/"
 MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_ROOT = BASE_DIR / "media"
-MEDIA_URL = "/media/"
 DATA_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024  # 10MB
 FILE_UPLOAD_MAX_MEMORY_SIZE = 10 * 1024 * 1024
 LOGIN_URL = "login"
