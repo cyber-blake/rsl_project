710f5d6b818fa34c9431ffe695c918b3f88a1007
Dmitry <blakethespend@gmail.com>
2026-03-16 10:24:03 +0300

    Revert "changed folders to app_nameApp"
    This reverts commit 326b588d2d286725af54d726c610a5a7cbf5eb7d.

--- rsl/rsl/urls.py
+++ rsl/rsl/urls.py
@@ -16,11 +16,11 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from helloApp import views
+from hello import views
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
-from authorizationApp import urls
+from authorization import urls
 
 urlpatterns = [
     path("", views.index, name="index"),
